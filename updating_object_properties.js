8019c743d6a07cf38483271246ad4fe226f61029
Suliman Fayyaz <hydrajin@gmail.com>
2022-07-11 23:56:46 -0400

    Add updating object properties

---
+++ updating_object_properties.js
@@ -0,0 +1,15 @@
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+
+// Dot Notation
+myDog.name = "Happy Coder";
+
+// Bracket Notation
+myDog["name"] = "Happy Coder";
