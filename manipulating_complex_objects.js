87dc4dd233efb3ccaf3d9af04d40ff82df368b95
Suliman Fayyaz <hydrajin@gmail.com>
2022-07-14 23:57:17 -0400

    Add manipulating complex objects

---
+++ manipulating_complex_objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true,
+  },
+  {
+    "artist": "dude",
+    "title": "doot",
+    "release_year": 2022,
+    "formats": [
+      "8-track",
+      "Vinyl LP"
+    ],
+    "gold": false
+  }
+];
